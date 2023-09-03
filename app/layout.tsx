f0b4fef6e326dbe75ea6827de71012e825726878
Federico Giorgino <grgfdrc@gmail.com>
2023-09-03 14:50:52 +0200

    Updated .gitignore

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import { TwIndicator } from "@/components/tw-indicator";
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter, Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
