98c30b1c87efd52c8a4a4d88c2ef079a68b44bb4
Federico Giorgino <grgfdrc@gmail.com>
2023-09-03 16:14:38 +0200

    Sidebar + Links/Logout

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -1,5 +1,7 @@
+import { Sidebar } from "@/components/layout/sidebar";
 import { TwIndicator } from "@/components/tw-indicator";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
@@ -17,16 +19,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html
-      lang="en"
-      className={cn(inter.className, "dark")}
-      style={{ colorScheme: "dark" }}
-    >
-      <head />
-      <body className="min-h-screen antialiased">
-        {children}
-        <TwIndicator />
-      </body>
-    </html>
+    <ClerkProvider>
+      <html
+        lang="en"
+        className={cn(inter.className, "dark")}
+        style={{ colorScheme: "dark" }}
+      >
+        <body className={`${inter.className} min-h-screen`}>
+          {/* <Navbar /> */}
+
+          <main className="flex flex-row">
+            <Sidebar />
+            <section className="flex min-h-screen flex-1 flex-col items-center px-6 pb-10 pt-24 max-md:pb-32 sm:px-10">
+              <div className="w-full">{children}</div>
+            </section>
+          </main>
+
+          {/* <Bottombar /> */}
+          <TwIndicator />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
