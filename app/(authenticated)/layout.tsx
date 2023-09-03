93549d9329701cfefb2c9938af88c15c7154b9d0
Federico Giorgino <grgfdrc@gmail.com>
2023-09-03 15:04:02 +0200

    Clerk Middleware

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -2,7 +2,7 @@ import { TwIndicator } from "@/components/tw-indicator";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
