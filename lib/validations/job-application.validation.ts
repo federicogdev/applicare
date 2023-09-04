b6a38e89a87f644540f9d8ae499240137b9cd5cf
Federico Giorgino <grgfdrc@gmail.com>
2023-09-04 13:01:37 +0200

    JobApplicationValidation + Type

---
+++ lib/validations/job-application.validation.ts
@@ -0,0 +1,15 @@
+import * as z from "zod";
+
+export const JobApplicationValidation = z.object({
+  company: z.string().nonempty().min(3, { message: "Minimum 3 characters." }),
+  position: z.string().nonempty().min(3, { message: "Minimum 3 characters." }),
+  location: z.string().nonempty().min(3, { message: "Minimum 3 characters." }),
+  status: z.enum(["PENDING", "INTERVIEW", "DECLINED"]),
+  type: z.enum(["FULL_TIME", "PART_TIME", "INTERNSHIP"]),
+  priority: z.enum(["LOW", "MEDIUM", "HIGH"]),
+  description: z.string().nonempty(),
+});
+
+export type JobApplicationValidationType = z.infer<
+  typeof JobApplicationValidation
+>;
